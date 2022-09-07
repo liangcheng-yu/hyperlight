e294e9be084793e04de74a7454ef58bb5d18de66
Simon Davies <simongdavies@users.noreply.github.com>
2022-09-07 15:34:41 +0100

    fix commands
    Signed-off-by: Simon Davies <simongdavies@users.noreply.github.com>

---
+++ src/hyperlight_host/scripts/valgrind-command.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -euo pipefail
+
+LD_LIBRARY_PATH=../../target/$1 valgrind --leak-check=full --error-exitcode=1 -s --suppressions=./minimal.supp ./bin/test_c.out --log-visible info --show-stderr
