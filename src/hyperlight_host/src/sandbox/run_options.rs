296b0ff2e5964a043fdd1bdf95dc9e1f1217175d
Dan Chiarlone <danilochiarlone@gmail.com>
2023-08-16 17:00:40 +0000

    typo
    Signed-off-by: Dan Chiarlone <danilochiarlone@gmail.com>

--- src/hyperlight_host/src/sandbox/run_options.rs
+++ src/hyperlight_host/src/sandbox/run_options.rs
@@ -14,7 +14,7 @@ bitflags! {
         /// Recycle the sandbox after running
         const RECYCLE_AFTER_RUN = 0b00000010;
         /// Run from guest binary (windows only)
-        #[cfg(windows)]
+        #[cfg(os = "windows")]
         const RUN_FROM_GUEST_BINARY =0b00000100;
     }
 }
