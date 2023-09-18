2d77405c1cc2a901848369e972526f7d2358826e
Dan Chiarlone <danilochiarlone@gmail.com>
2023-09-18 23:22:55 +0000

    managed to make a call into the HyperlightGuest lib
    Signed-off-by: Dan Chiarlone <danilochiarlone@gmail.com>

--- src/tests/rust_guests/simpleguest/src/lib.rs
+++ src/tests/rust_guests/simpleguest/src/lib.rs
@@ -4,4 +4,10 @@
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn HyperlightMain() {
+    // try to use GetFlatBufferResultFromInt to make sure we can communicate w/ the HyperlightGuest lib
+    let _result = unsafe { GetFlatBufferResultFromInt(42) };
+}
+
+extern "C" {
+    pub fn GetFlatBufferResultFromInt(value: u32) -> *const u8;
 }
