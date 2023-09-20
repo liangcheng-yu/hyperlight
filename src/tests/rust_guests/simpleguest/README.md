a4da7172f5bbb625a072564eb316741d013c558f
Dan Chiarlone <danilochiarlone@gmail.com>
2023-09-20 03:39:15 +0000

    ported smallVar to simpleguest-rs
    Signed-off-by: Dan Chiarlone <danilochiarlone@gmail.com>

--- src/tests/rust_guests/simpleguest/README.md
+++ src/tests/rust_guests/simpleguest/README.md
@@ -6,10 +6,10 @@ This is simpleguest, but written in rust. Here's how to build it:
 # on Windows:
 cargo build --release --target x86_64-pc-windows-msvc
 
-link.exe /NOLOGO /NXCOMPAT /SAFESEH:NO /RELEASE /ENTRY:"entryPoint" /SUBSYSTEM:NATIVE .\target\x86_64-pc-windows-msvc\release\libsimpleguest.rlib /LIBPATH:"C:\Users\danbugs\repos\hyperlight\x64\debug\" /OUT:"C:\Users\danbugs\repos\hyperlight\src\tests\Guests\simpleguest\x64\debug\simpleguest_new.exe" /ERRORREPORT:NONE /ALIGN:512 /NODEFAULTLIB /HEAP:"131072,131072" /DYNAMICBASE "HyperlightGuest.lib" /STACK:"65536,65536" /MACHINE:X64
+link.exe /NOLOGO /NXCOMPAT /SAFESEH:NO /RELEASE /ENTRY:"entryPoint" /SUBSYSTEM:NATIVE .\target\x86_64-pc-windows-msvc\release\libsimpleguest.rlib /LIBPATH:"..\..\..\..\x64\debug" /OUT:"..\..\Guests\simpleguest\x64\debug\simpleguest_new.exe" /ERRORREPORT:NONE /ALIGN:512 /NODEFAULTLIB /HEAP:"131072,131072" /DYNAMICBASE "HyperlightGuest.lib" /STACK:"65536,65536" /MACHINE:X64
 
 # on Linux:
 cargo build --release --target x86_64-pc-windows-gnu
 
-lld-link-10  /nxcompat /safeseh:no /entry:entryPoint /subsystem:native ./target/x86_64-pc-windows-gnu/release/libsimpleguest.rlib /libpath:../../../../x64/debug /OUT:../../Guests/simpleguest/x64/debug/simpleguest_new.exe  /align:512 /nodefaultlib /heap:131072,131072 /dynamicbase "HyperlightGuest.lib" /stack:65536,65536 /machine:x64
+lld-link-10 /nologo /nxcompat /safeseh:no /entry:entryPoint /subsystem:native ./target/x86_64-pc-windows-gnu/release/libsimpleguest.rlib /libpath:../../../../x64/debug /OUT:../../Guests/simpleguest/x64/debug/simpleguest_new.exe  /ERRORREPORT:NONE /align:512 /nodefaultlib /heap:131072,131072 /dynamicbase "HyperlightGuest.lib" /stack:65536,65536 /machine:x64
 ```
--- src/tests/rust_guests/simpleguest/src/lib.rs
+++ src/tests/rust_guests/simpleguest/src/lib.rs
@@ -4,10 +4,47 @@
 #[allow(non_snake_case)]
 #[no_mangle]
 pub extern "C" fn HyperlightMain() {
-    // try to use GetFlatBufferResultFromInt to make sure we can communicate w/ the HyperlightGuest lib
-    let _result = unsafe { GetFlatBufferResultFromInt(42) };
+    // - manually register smallVar
+
+    // get name
+    let smallVarName: [u8; 9] = *b"smallVar\0";
+    let smallVarNamePtr: *const u8 = &smallVarName as *const u8;
+
+    // get params
+    let smallParameterKind: [u8; 1] = [0];
+    let smallParameterKindPtr: *const u8 = &smallParameterKind as *const u8;
+
+    // create fxn def
+    let smallVarDefinition = unsafe { CreateFunctionDefinition(smallVarNamePtr, smallVar, 0, smallParameterKindPtr) };
+
+    // register fxn def
+    unsafe { RegisterFunction(smallVarDefinition) };
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "C" fn smallVar() -> *const u8 {
+    let _buffer: [u8; 1024] = [0; 1024];
+    return unsafe { GetFlatBufferResultFromInt(1024) };
 }
 
 extern "C" {
+    #[allow(non_snake_case)]
     pub fn GetFlatBufferResultFromInt(value: u32) -> *const u8;
+
+    #[allow(non_snake_case)]
+    pub fn CreateFunctionDefinition(functionName: *const u8, pFunction: extern "C" fn() -> *const u8, paramCount: i32, parameterKind: *const u8) -> i32;
+    // ^^^ In C, this function returns ns(GuestFunctionDefinition_ref_t),
+    // which is utilizing a macro from flatbuffers to expand to 
+    // Hyperlight_Generated_GuestFunctionDefinition_ref_t,
+    // which, in itself, is flatbuffers_ref_t,
+    // which is just a typedef for flatcc_builder_ref_t
+    // which is a wrapper for flatcc_builder_ref_t
+    // that expands to flatbuffers_soffset_t
+    // which, finally, is a typedef for int32_t.
+
+    #[allow(non_snake_case)]
+    pub fn RegisterFunction(functionDefinition: i32);
+    // ^^^ In C, this function takes ns(GuestFunctionDefinition_ref_t),
+    // but we can follow the same logic as the function above to use i32 instead.
 }
