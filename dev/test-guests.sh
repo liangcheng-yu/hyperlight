826b84c45d5ae34d0414e055b5d0c92708e24421
Simon Davies <simongdavies@users.noreply.github.com>
2023-11-16 22:41:34 +0000

    fix test-guests.sh (#1012)
    Signed-off-by: Simon Davies <simongdavies@users.noreply.github.com>

--- dev/test-guests.sh
+++ dev/test-guests.sh
@@ -4,15 +4,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+SCRIPT_DIR=$(realpath $(dirname "$0"))
+REPO_DIR=$(realpath ${SCRIPT_DIR}/..)
+pushd $REPO_DIR
+
 # set this to the release tag you want to download the test guests from
 # See https://github.com/deislabs/hyperlight/releases
 RELEASE_TAG="${RELEASE_TAG:=latest}"
-mkdir -p src/tests/Guests/dummyguest/x64/debug/ && cd  src/tests/Guests/dummyguest/x64/debug/ && gh release download  ${RELEASE_TAG} -p 'dummyguest.exe' --clobber && cd -
-mkdir -p src/tests/Guests/dummyguest/x64/release/ && cd  src/tests/Guests/dummyguest/x64/release/ && gh release download  ${RELEASE_TAG} -p 'dummyguest.exe' --clobber && cd -
-mkdir -p src/tests/Guests/simpleguest/x64/debug/ && cd  src/tests/Guests/simpleguest/x64/debug/ && gh release download  ${RELEASE_TAG} -p 'simpleguest.exe' --clobber && cd -
-mkdir -p src/tests/Guests/simpleguest/x64/release/ && cd  src/tests/Guests/simpleguest/x64/release/ && gh release download  ${RELEASE_TAG} -p 'simpleguest.exe' --clobber && cd -
-mkdir -p src/tests/Guests/callbackguest/x64/debug/ && cd  src/tests/Guests/callbackguest/x64/debug/ && gh release download  ${RELEASE_TAG} -p 'callbackguest.exe' --clobber && cd -
-mkdir -p src/tests/Guests/callbackguest/x64/release/ && cd  src/tests/Guests/callbackguest/x64/release/ && gh release download  ${RELEASE_TAG} -p 'callbackguest.exe' --clobber && cd -
+mkdir -p src/tests/Guests/dummyguest/x64/debug/ && cd  src/tests/Guests/dummyguest/x64/debug/ && gh release download  ${RELEASE_TAG} -p 'dummyguest.exe' --clobber
+cd $REPO_DIR
+mkdir -p src/tests/Guests/dummyguest/x64/release/ && cd  src/tests/Guests/dummyguest/x64/release/ && gh release download  ${RELEASE_TAG} -p 'dummyguest.exe' --clobber
+cd $REPO_DIR
+mkdir -p src/tests/Guests/simpleguest/x64/debug/ && cd  src/tests/Guests/simpleguest/x64/debug/ && gh release download  ${RELEASE_TAG} -p 'simpleguest.exe' --clobber
+cd $REPO_DIR
+mkdir -p src/tests/Guests/simpleguest/x64/release/ && cd  src/tests/Guests/simpleguest/x64/release/ && gh release download  ${RELEASE_TAG} -p 'simpleguest.exe' --clobber
+cd $REPO_DIR
+mkdir -p src/tests/Guests/callbackguest/x64/debug/ && cd  src/tests/Guests/callbackguest/x64/debug/ && gh release download  ${RELEASE_TAG} -p 'callbackguest.exe' --clobber
+cd $REPO_DIR
+mkdir -p src/tests/Guests/callbackguest/x64/release/ && cd  src/tests/Guests/callbackguest/x64/release/ && gh release download  ${RELEASE_TAG} -p 'callbackguest.exe' --clobber
+cd $REPO_DIR
 
 mkdir -p src/tests/Hyperlight.Tests/bin/debug/net6.0
 mkdir -p src/tests/Hyperlight.Tests/bin/release/net6.0
@@ -23,3 +33,5 @@ cp src/tests/Guests/simpleguest/x64/debug/simpleguest.exe src/tests/Hyperlight.T
 cp src/tests/Guests/simpleguest/x64/release/simpleguest.exe src/tests/Hyperlight.Tests/bin/release/net6.0
 cp src/tests/Guests/callbackguest/x64/debug/callbackguest.exe src/tests/Hyperlight.Tests/bin/debug/net6.0
 cp src/tests/Guests/callbackguest/x64/release/callbackguest.exe src/tests/Hyperlight.Tests/bin/release/net6.0
+
+popd
