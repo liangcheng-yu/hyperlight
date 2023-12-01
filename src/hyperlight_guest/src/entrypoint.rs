b0bdb68da2610826757cd8790cf412522c60fe1c
danbugs <danilochiarlone@gmail.com>
2023-11-30 16:51:26 -0800

    fix && fmt
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- src/hyperlight_guest/src/entrypoint.rs
+++ src/hyperlight_guest/src/entrypoint.rs
@@ -21,49 +21,49 @@ extern "C" {
 #[no_mangle]
 pub extern "C" fn entrypoint(peb_address: i64, _seed: i64, ops: i32) -> i32 {
     unsafe {
-    if peb_address == 0 {
-        return -1;
-    }
+        if peb_address == 0 {
+            return -1;
+        }
 
-    P_PEB = Some(peb_address as *mut HyperlightPEB);
+        P_PEB = Some(peb_address as *mut HyperlightPEB);
 
-    let peb_ptr = P_PEB.unwrap();
+        let peb_ptr = P_PEB.unwrap();
 
-    let heap_start = (*peb_ptr).guestheapData.guestHeapBuffer as usize;
-    let heap_size = (*peb_ptr).guestheapData.guestHeapSize as usize;
-    HEAP_ALLOCATOR.lock().init(heap_start, heap_size);
+        let heap_start = (*peb_ptr).guestheapData.guestHeapBuffer as usize;
+        let heap_size = (*peb_ptr).guestheapData.guestHeapSize as usize;
+        HEAP_ALLOCATOR.lock().init(heap_start, heap_size);
 
-    // In C, at this point, we call __security_init_cookie.
-    // That's a dependency on MSVC, which we can't utilize here.
-    // This is to protect against buffer overflows in C, which
-    // are inherently protected in Rust.
+        // In C, at this point, we call __security_init_cookie.
+        // That's a dependency on MSVC, which we can't utilize here.
+        // This is to protect against buffer overflows in C, which
+        // are inherently protected in Rust.
 
-    // In C, here, we have a `if (!setjmp(jmpbuf))`, which is used in case an error occurs
-    // because longjmp is called, which will cause execution to return to this point to
-    // halt the program. In Rust, we don't have or need this sort of error handling as the
-    // language relies on specific structures like `Result`, and `?` that allow for
-    // propagating up the call stack.
+        // In C, here, we have a `if (!setjmp(jmpbuf))`, which is used in case an error occurs
+        // because longjmp is called, which will cause execution to return to this point to
+        // halt the program. In Rust, we don't have or need this sort of error handling as the
+        // language relies on specific structures like `Result`, and `?` that allow for
+        // propagating up the call stack.
 
-    OS_PAGE_SIZE = ops as u32;
+        OS_PAGE_SIZE = ops as u32;
 
-    let outb_ptr: fn(u16, u8) = core::mem::transmute((*peb_ptr).pOutb);
-    OUTB_PTR = Some(outb_ptr as fn(u16, u8));
+        let outb_ptr: fn(u16, u8) = core::mem::transmute((*peb_ptr).pOutb);
+        OUTB_PTR = Some(outb_ptr as fn(u16, u8));
 
-    let outb_ptr_with_context: fn(*mut c_void, u16, u8) =
-        core::mem::transmute((*peb_ptr).pOutb);
-    OUTB_PTR_WITH_CONTEXT = Some(outb_ptr_with_context as fn(*mut c_void, u16, u8));
+        let outb_ptr_with_context: fn(*mut c_void, u16, u8) =
+            core::mem::transmute((*peb_ptr).pOutb);
+        OUTB_PTR_WITH_CONTEXT = Some(outb_ptr_with_context as fn(*mut c_void, u16, u8));
 
-    if (*peb_ptr).pOutb.is_null() {
-        RUNNING_IN_HYPERLIGHT = true;
-    }
+        if (*peb_ptr).pOutb.is_null() {
+            RUNNING_IN_HYPERLIGHT = true;
+        }
 
-    (*peb_ptr).guest_function_dispatch_ptr = dispatch_function as usize as u64;
+        (*peb_ptr).guest_function_dispatch_ptr = dispatch_function as usize as u64;
 
-    reset_error();
+        reset_error();
 
-    hyperlight_main();
+        hyperlight_main();
 
-    finalise_function_table();
+        finalise_function_table();
     }
 
     halt();
