8d0c6eae3f0c15bd3b0d31e3689f81512bc5e080
Mark Rossetti <marosset@microsoft.com>
2024-03-26 07:53:34 -0700

    Publishing includes for c guestlib in a new crate (#1252)
    Co-authored-by: Dan Chiarlone <dchiarlone@microsoft.com>

---
+++ src/HyperlightGuest/src/lib.rs
@@ -0,0 +1,36 @@
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "."]
+#[include = "*.h"]
+#[exclude = "target/*.h"]
+struct Assets;
+
+/// Extract_files extracts all of the header files embedded in this crate
+/// to the specified folder preserving the folder structure.
+/// We are doing this so we can consume these header files in other Rust projects.
+pub fn extract_files(p: &Path) -> Result<(), String> {
+    if !Path::new(p).exists() {
+        return Err::<(), String>(format!("{} does not exist", p.display()))
+    }
+
+    for f in Assets::iter() {
+        let embedded_path = f.as_ref();
+        let full_path = p.join(embedded_path);
+        let parent_dir = full_path.parent().unwrap();
+
+        if !(parent_dir.exists()) {
+            let _ = std::fs::create_dir_all(parent_dir);
+        }
+        println!("writing {}", parent_dir.display());
+
+        let mut target = File::create(&full_path).unwrap();
+        let embedded_file = Assets::get(embedded_path).unwrap();
+        target.write_all(embedded_file.data.as_ref()).unwrap();
+    }
+
+    Ok(())
+}
