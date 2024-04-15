844ea1b927aeb4123509819e658ee305694b5b2c
Ludvig Liljenberg <lliljenberg@microsoft.com>
2024-04-15 00:52:32 -0700

    Add chrome-tracer example (#1287)

---
+++ src/hyperlight_host/examples/chrome-tracing/main.rs
@@ -0,0 +1,43 @@
+use hyperlight_host::{
+    func::{ParameterValue, ReturnType, ReturnValue},
+    sandbox::uninitialized::UninitializedSandbox,
+    sandbox_state::{sandbox::EvolvableSandbox, transition::Noop},
+    GuestBinary, MultiUseSandbox, Result,
+};
+use hyperlight_testing::simple_guest_as_string;
+use tracing_chrome::ChromeLayerBuilder;
+use tracing_subscriber::prelude::*;
+
+// This example can be run with `cargo run --package hyperlight_host --example chrome-tracing --release`
+fn main() -> Result<()> {
+    // set up tracer
+    let (chrome_layer, _guard) = ChromeLayerBuilder::new().build();
+    tracing_subscriber::registry().with(chrome_layer).init();
+
+    let simple_guest_path =
+        simple_guest_as_string().expect("Cannot find the guest binary at the expected location.");
+
+    // Create a new sandbox.
+    let usandbox =
+        UninitializedSandbox::new(GuestBinary::FilePath(simple_guest_path), None, None, None)?;
+
+    // NOTE: if replacing MultiUseSandbox with SingleUseSandbox, the function call will take ~50x longer because the drop
+    // happens inside `call_guest_function_by_name` rather than at the end of of this `main` block.
+    //
+    // Another way to see the difference is to rename the unused variable below `_try_rename_this` to `_`, which will have the same effect
+    let sbox = usandbox
+        .evolve(Noop::<UninitializedSandbox, MultiUseSandbox>::default())
+        .unwrap();
+
+    // do the function call
+    let current_time = std::time::Instant::now();
+    let (_try_rename_this, res) = sbox.call_guest_function_by_name(
+        "Echo",
+        ReturnType::String,
+        Some(vec![ParameterValue::String("Hello, World!".to_string())]),
+    )?;
+    let elapsed = current_time.elapsed();
+    println!("Function call finished in {:?}.", elapsed);
+    assert!(matches!(res, ReturnValue::String(s) if s == "Hello, World!"));
+    Ok(())
+}
