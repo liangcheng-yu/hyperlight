5afcc721fac32675ff3d3951c9669e8e9044ea15
Lucy Menon <168595099+syntactically@users.noreply.github.com>
2024-08-28 23:51:28 +0100

    Prefer #!/usr/bin/env bash to #!/bin/bash
    In certain environments, /bin/bash does not exist, and bash should be
    discovered via $PATH. While we do not support these environments, and
    /bin/bash does exist on our supported Mariner installations, this is a
    minimal change and aligns these scripts with other we have that
    already use the /usr/bin/env bash approach.

--- src/hyperlight_capi/scripts/test-command.sh
+++ src/hyperlight_capi/scripts/test-command.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 LD_LIBRARY_PATH=../../target/$1 ./bin/test_c.out 
--- src/hyperlight_capi/scripts/valgrind-command.sh
+++ src/hyperlight_capi/scripts/valgrind-command.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
 GEN_SUPPRESSIONS=""
