b2880b6c1eda15fb73f874e534ded5cc36f963c5
Ludvig Liljenberg <lliljenberg@microsoft.com>
2024-10-03 10:54:31 -0700

    remove cargo valgrind tests, remove valgrind suppressions (#1573)

--- src/hyperlight_capi/scripts/valgrind-command.sh
+++ src/hyperlight_capi/scripts/valgrind-command.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-GEN_SUPPRESSIONS=""
-if [[ "${GEN_SUPPRESSIONS_ON:-1}" == "1" ]]; then
-    GEN_SUPPRESSIONS="--gen-suppressions=all"
-fi
-
-LOG_FILE=""
-if [[ "${LOG_FILE_ON:-0}" == "1" ]]; then
-    LOG_FILE="--log-file=minimalraw.log"
-fi
-
 LD_LIBRARY_PATH=../../target/$1 \
-valgrind ${GEN_SUPPRESSIONS} ${LOG_FILE} \
+valgrind \
 --leak-check=full \
 --error-exitcode=1 \
 --track-origins=yes \
---sim-hints=lax-ioctls \
 -s \
---suppressions=./valgrind_suppressions/minimal.supp \
---suppressions=./valgrind_suppressions/hyperv_linux.supp \
---suppressions=./valgrind_suppressions/prometheus.supp \
 ./bin/test_c.out \
 --log-visible info \
 --show-stderr
